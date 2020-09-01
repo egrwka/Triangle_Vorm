06bb787060c3ebffd4a863597cad9f1f8a2ef23d
Opilane TTHK <opilane@tthk.ee>
2020-09-01 11:56:27 +0300

    Add project files.

---
+++ Triangle_Vorm/Triangle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Vorm
+{
+    class Triangle
+    {
+        public double a; // 1st stroka
+        public double b; // 2st storka
+        public double c; // 3th stroka
+        public Triangle(double A, double B, double C) // Konstryktor
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+
+        public double Perimeter()
+        {
+            double p = 0;
+            p = a + b + c;
+            return p;
+        }
+
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+
+        public double GetSetA
+        {
+            get
+            { return a; }
+            set
+            { a = value; }
+        }
+        public double GetSetB
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        public double GetSetC
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+
+        public bool ExistTriange
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return true;
+                else return false;
+            }
+        }
+    }
+
+
+}
